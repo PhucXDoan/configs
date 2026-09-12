da3ca0589467fcbefeac9210aa39a8283e3c5844
Phuc X. Doan <phucxdoan@gmail.com>
2026-09-12 14:23:55 +0000

    Updated.

---
+++ configuration.nix
@@ -0,0 +1,34 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [
+  ];
+
+  wsl.enable = true;
+  wsl.defaultUser = "nixos";
+
+  system.stateVersion = "26.05";
+
+  environment.systemPackages = with pkgs; [
+  ];
+
+  programs.git = {
+    enable = true;
+    config = {
+      user.name = "Phuc X. Doan";
+      user.email = "phucxdoan@gmail.com";
+    };
+  };
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+    configure = {
+      customRC = ''
+        luafile ~/configs/init.lua
+      '';
+    };
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "NixOS-WSL System Configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL";
+    nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, nixos-wsl, ... }: {
+    nixosConfigurations.phucxdoan = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        nixos-wsl.nixosModules.wsl
+        ./configuration.nix
+      ];
+    };
+  };
+}
